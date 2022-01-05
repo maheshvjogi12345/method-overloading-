33fb583e73665909d4504a654a80e2df63d1e1bd
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2022-01-05 09:45:52 +0530

    method overloading

---
+++ Rocket.java
@@ -0,0 +1,53 @@
+class Rocket{
+
+	static void missiles(){
+		System.out.println("gunpowder");
+	}
+	
+	static void missiles(int time){
+		System.out.println("fireworks");
+		
+	} 
+	static void missiles(int details, String acceleter){
+		System.out.println("vaccumblaster");
+	}
+	static void missiles(String piston, int cylinder){
+		System.out.println("jetblast");
+	}
+	static void launching(){
+		System.out.println("torpedoes");
+	}
+	static void launching(int topmusic){
+		System.out.println("saturnv");
+	} 
+	static void launching(int topmusic2, String topmusic3){
+		System.out.println("spacerockets");
+	}
+	static void launching(String topmusic4, int topmusic5){
+		System.out.println("sattelites");
+	}
+	static void testing(){
+		System.out.println("launchingtesting");
+	}
+	static void testing(int jkll){
+		System.out.println("sattelitestesting");
+	} 
+	static void testing(int qwer, String tyui){
+		System.out.println("propellertesting");
+	}
+	static void testing(String opas, int dfgh){
+		System.out.println("spacetesting");
+	}
+	static void rescueoperations(){
+		System.out.println("china");
+	}
+	static void rescueoperations(int location){
+		System.out.println("usa");
+	} 
+	static void rescueoperations(int location2, String location3){
+		System.out.println("japan");
+	}
+	static void rescueoperations(String location4, int location5){
+		System.out.println("africa");
+	}
+}
---
+++ RocketTester.java
@@ -0,0 +1,52 @@
+class RocketTester{
+	public static void main(String []args){
+		Rocket.missiles();
+		int time=13;
+		Rocket.missiles(time);
+		int details= 14; 
+		String acceleter="to 150 speed";
+		Rocket.missiles(details, acceleter);
+		String piston= "800 rpm"; 
+		int cylinder= 40;
+		Rocket.missiles(piston, cylinder);
+		System.out.println();
+		
+		System.out.println("launching======feature");
+		Rocket.launching();
+		int topmusic=13;
+		Rocket.launching(topmusic);
+		int topmusic2= 14; 
+		String topmusic3="to 150 speed";
+		Rocket.launching(topmusic2, topmusic3);
+		String topmusic4= "800 rpm";
+		int topmusic5= 40;
+		Rocket.launching(topmusic4, topmusic5);
+		System.out.println();
+		
+		System.out.println("testing======feature");
+		Rocket.testing();
+		int jkll=13;
+		Rocket.testing(jkll);
+		int qwer= 14; 
+		String tyui="to 150 speed";
+		Rocket.testing(qwer, tyui);
+		String opas= "800 rpm";
+		int dfgh= 40;
+		Rocket.testing(opas, dfgh);
+		System.out.println();
+		
+		System.out.println("rescueoperations======feature");
+		Rocket.rescueoperations();
+		int location=13;
+		Rocket.rescueoperations(location);
+		int location2= 14; 
+		String location3="to 150 speed";
+		Rocket.rescueoperations(location2, location3);
+		String location4= "800 rpm"; 
+		int location5= 40;
+		Rocket.rescueoperations(location4, location5);
+		
+	
+	}
+
+}
